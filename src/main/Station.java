36a36a2de11ceef11473b1d7cd0ee6000d226b75
CdoxHype <julio.camacho3@upr.edu>
2024-04-02 17:20:55 -0300

    StationJava

--- src/main/Station.java
+++ src/main/Station.java
@@ -2,21 +2,25 @@ package main;
 
 public class Station {
 	
+	String cityName;
+	int distance = 0;
 	
 	public Station(String name, int dist) {
+		this.cityName = name;
+		this.distance = dist;
 	}
 	
 	public String getCityName() {
-		
+		return this.cityName;
 	}
 	public void setCityName(String cityName) {
-		
+		this.cityName = cityName;
 	}
 	public int getDistance() {
-		
+		return this.distance;
 	}
 	public void setDistance(int distance) {
-		
+		this.distance = distance;
 	}
 
 	@Override
